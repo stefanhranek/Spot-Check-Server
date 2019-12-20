56444d8b24f9cf90652192068ee8332c911906fd
stefan hranek <hranekstefan@gmail.com>
2019-12-20 09:20:58 +0100

    final heroku commit v2

---
+++ public/precache-manifest.3c27224b6257db8dfcceb92ae143e8af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd8d32d1b7e1b2728d53d8d33d57fd47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "073f71bbeff535075c05",
+    "url": "/static/css/main.04f87eff.chunk.css"
+  },
+  {
+    "revision": "007d1ce644414257883a",
+    "url": "/static/js/2.23f6e242.chunk.js"
+  },
+  {
+    "revision": "7b9b0a5c1ac3c7756371036df8d98f81",
+    "url": "/static/js/2.23f6e242.chunk.js.LICENSE"
+  },
+  {
+    "revision": "073f71bbeff535075c05",
+    "url": "/static/js/main.7cd12b1a.chunk.js"
+  },
+  {
+    "revision": "5bba74af4a4e52b458f9",
+    "url": "/static/js/runtime-main.e8ac4ea3.js"
+  }
+]);
